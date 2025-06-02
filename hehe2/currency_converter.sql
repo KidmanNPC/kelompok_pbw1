dc8a08842a5fc5b2612d6f83186727183da6d21b
Nate <127719222+KidmanNPC@users.noreply.github.com>
2025-06-02 23:28:03 +0700

    Add files via upload

---
+++ hehe2/currency_converter.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 02, 2025 at 04:47 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `currency_converter`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `conversion_history`
+--
+
+CREATE TABLE `conversion_history` (
+  `id` int(11) NOT NULL,
+  `amount` decimal(10,2) DEFAULT NULL,
+  `from_currency` varchar(10) DEFAULT NULL,
+  `to_currency` varchar(10) DEFAULT NULL,
+  `result` decimal(10,2) DEFAULT NULL,
+  `converted_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `conversion_history`
+--
+ALTER TABLE `conversion_history`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `conversion_history`
+--
+ALTER TABLE `conversion_history`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
